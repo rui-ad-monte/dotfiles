38be5b57d30b86e63c993754b3a023ffa9fbe4d7
Rui Adelino Monte <38920989+rui-ad-monte@users.noreply.github.com>
2025-07-16 23:08:21 +0100

    disable scroll animation and tabs

---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,7 @@
+return {
+  -- Disable bufferline (tabs)
+  { "akinsho/bufferline.nvim", enabled = false },
+
+  -- Disable scroll animation plugin (example: 'petertriho/nvim-scrollbar')
+  { "petertriho/nvim-scrollbar", enabled = false },
+}
